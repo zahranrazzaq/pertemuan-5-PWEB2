f9828dc936e77dc89e0616ac4379934ca932c1c9
zahranrazzaq <zahranrazzaq797@gmail.com>
2025-04-15 09:35:14 -0700

    Add files via upload

---
+++ form_prodi.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+ 
+ <form method="POST" action="proses_prodi.php">
+   <div class="form-group row">
+     <label for="" class="col-4 col-form-label">Kode</label> 
+     <div class="col-8">
+       <div class="input-group">
+         <div class="input-group-prepend">
+           <div class="input-group-text">
+             <i class="fa fa-address-card"></i>
+           </div>
+         </div> 
+         <input id="" name="" type="text" class="form-control">
+       </div>
+     </div>
+   </div>
+   <div class="form-group row">
+     <label for="text" class="col-4 col-form-label">Nama Prodi</label> 
+     <div class="col-8">
+       <input id="text" name="text" type="text" class="form-control">
+     </div>
+   </div>
+   <div class="form-group row">
+     <label for="text1" class="col-4 col-form-label">Kaprodi</label> 
+     <div class="col-8">
+       <input id="text1" name="text1" type="text" class="form-control">
+     </div>
+   </div> 
+   <div class="form-group row">
+     <div class="offset-4 col-8">
+       <input name="proses" type="submit" value="Simpan" class="btn btn-primary">
+     </div>
+   </div>
+ </form>
